b0c4c4a84147791195fac56121a98651250bf2ca
Blair Mitchelmore <blair@mitchelmore.ca>
2021-12-01 17:59:40 -0500

    Initial Commit

---
+++ AOC2021/AOC2021App.swift
@@ -0,0 +1,17 @@
+//
+//  AOC2021App.swift
+//  AOC2021
+//
+//  Created by Blair Mitchelmore on 2021-12-01.
+//
+
+import SwiftUI
+
+@main
+struct AOC2021App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
