710410784ecbe49f47ccd0e8fd02ed3045806d34
Blair Mitchelmore <blair@mitchelmore.ca>
2021-12-01 19:39:25 -0500

    Add day 1 solutions

--- AOC2021/ContentView.swift
+++ AOC2021/ContentView.swift
@@ -7,10 +7,90 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct PromptSheet: View {
+    @Environment(\.dismiss) var dismiss: DismissAction
+    @Binding var textValue: String
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TextEditor(text: _textValue)
+            .frame(width: 400, height: 300, alignment: .center)
+        Button("Submit") {
+            dismiss()
+        }
+    }
+}
+
+enum Puzzle {
+    case Day1Puzzle1
+    case Day1Puzzle2
+    
+    var section: String {
+        switch self {
+        case .Day1Puzzle1, .Day1Puzzle2:
+            return "Day 1"
+        }
+    }
+    
+    var title: String {
+        switch self {
+        case .Day1Puzzle1:
+            return "Puzzle 1"
+        case .Day1Puzzle2:
+            return "Puzzle 2"
+        }
+    }
+    
+    func solve(input: String) throws -> Int {
+        switch self {
+        case .Day1Puzzle1:
+            let puzzle = try AOC2021.Day1Puzzle1(contents: input)
+            let answer = puzzle.answer()
+            return answer
+        case .Day1Puzzle2:
+            let puzzle = try AOC2021.Day1Puzzle2(contents: input)
+            let answer = puzzle.answer()
+            return answer
+        }
+    }
+}
+
+struct ContentView: View {
+    @State var readingInput: Bool = false
+    @State var inputString: String = ""
+    @State var currentPuzzle: Puzzle?
+    
+    private func solve(_ input: String) {
+        guard let puzzle = currentPuzzle else { return }
+        do {
+            let answer = try puzzle.solve(input: input)
+            print("Answer: \(answer)")
+        } catch {
+            print("Error!")
+            print(error)
+        }
+    }
+    
+    var items: [[Puzzle]] = [
+        [.Day1Puzzle1, .Day1Puzzle2]
+    ]
+    
+    var body: some View {
+        List(items, id: \.self) { item in
+            Section(item.first!.section) {
+                ForEach(item, id: \.self) { item in
+                    Text(item.title)
+                        .sheet(isPresented: $readingInput) {
+                            solve(inputString)
+                        } content: {
+                            PromptSheet(textValue: $inputString)
+                        }
+                        .onTapGesture {
+                            currentPuzzle = item
+                            readingInput = true
+                        }
+                }
+            }
+        }.listStyle(.sidebar)
     }
 }
 
---
+++ AOC2021/Day1.swift
@@ -0,0 +1,77 @@
+//
+//  Day1Puzzle1.swift
+//  AOC2021
+//
+//  Created by Blair Mitchelmore on 2021-12-01.
+//
+
+import Foundation
+
+enum AOCError: Error {
+    case invalidInput
+}
+
+extension Collection {
+    var pairs: [(Element, Element)] {
+        return Array(zip(self, self.dropFirst()))
+    }
+    var triples: [(Element, Element, Element)] {
+        guard count > 2 else { return [] }
+        var triples: [(Element, Element, Element)] = []
+        var a = Array(self)
+        var b = Array(self.dropFirst())
+        var c = Array(self.dropFirst(2))
+        while !a.isEmpty, !b.isEmpty, !c.isEmpty {
+            triples.append((a.first!, b.first!, c.first!))
+            a.removeFirst()
+            b.removeFirst()
+            c.removeFirst()
+        }
+        return triples
+    }
+}
+
+private func parse(input: String) throws -> [Int] {
+    return try input
+        .split(separator: "\n")
+        .map { input -> Int in
+            guard let value = Int(input, radix: 10) else {
+                throw AOCError.invalidInput
+            }
+            return value
+        }
+}
+
+struct Day1Puzzle1 {
+    private let input: [Int]
+    
+    init(contents: String) throws {
+        input = try parse(input: contents)
+    }
+    
+    func answer() -> Int {
+        return input
+            .pairs
+            .map { $0.1 - $0.0 }
+            .filter { $0 > 0 }
+            .count
+    }
+}
+
+struct Day1Puzzle2 {
+    private let input: [Int]
+    
+    init(contents: String) throws {
+        input = try parse(input: contents)
+    }
+    
+    func answer() -> Int {
+        return input
+            .triples
+            .map { $0.0 + $0.1 + $0.2 }
+            .pairs
+            .map { $0.1 - $0.0 }
+            .filter { $0 > 0 }
+            .count
+    }
+}
